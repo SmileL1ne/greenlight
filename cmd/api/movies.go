c3b4a5b430af3a47bdff834d2ed330df36ef3802
mustafa <mustafa@kwaaka.com>
2024-05-20 00:46:53 +0500

    feat: add crud

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
-	"time"
 
 	"greenlight.mustik.net/internal/data"
 	"greenlight.mustik.net/internal/validator"
@@ -59,16 +59,98 @@ func (app *application) showMovieHandler(w http.ResponseWriter, req *http.Reques
 		return
 	}
 
-	movie := data.Movie{
-		ID:        id,
-		CreatedAt: time.Now(),
-		Title:     "Harry Potter",
-		Runtime:   144,
-		Genres:    []string{"drama", "romance", "fantasy"},
-		Version:   1,
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, req)
+		default:
+			app.serverErrorResponse(w, req, err)
+		}
+		return
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, req, err)
+	}
+}
+
+func (app *application) updateMovieHandler(w http.ResponseWriter, req *http.Request) {
+	id, err := app.readIDParam(req)
+	if err != nil {
+		app.notFoundResponse(w, req)
+		return
+	}
+
+	movie, err := app.models.Movies.Get(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, req)
+		default:
+			app.serverErrorResponse(w, req, err)
+		}
+		return
+	}
+
+	var input struct {
+		Title   string       `json:"title"`
+		Year    int32        `json:"year"`
+		Runtime data.Runtime `json:"runtime"`
+		Genres  []string     `json:"genres"`
+	}
+
+	err = app.readJSON(w, req, &input)
+	if err != nil {
+		app.badRequestResponse(w, req, err)
+		return
+	}
+
+	movie.Title = input.Title
+	movie.Year = input.Year
+	movie.Runtime = input.Runtime
+	movie.Genres = input.Genres
+
+	v := validator.New()
+
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, req, v.Errors)
+		return
+	}
+
+	err = app.models.Movies.Update(movie)
+	if err != nil {
+		app.serverErrorResponse(w, req, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, req, err)
+		return
+	}
+}
+
+func (app *application) deleteMovieHandler(w http.ResponseWriter, req *http.Request) {
+	id, err := app.readIDParam(req)
+	if err != nil {
+		app.notFoundResponse(w, req)
+		return
+	}
+
+	err = app.models.Movies.Delete(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, req)
+		default:
+			app.serverErrorResponse(w, req, err)
+		}
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"message": "movie successfully deleted"}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, req, err)
 	}
