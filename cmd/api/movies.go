0feba46d774461e545cb2929568deaf5f5386d10
mustafa <mustafa@kwaaka.com>
2024-05-24 01:18:24 +0500

    feat: add pagination and sorting

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -191,13 +191,13 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, req *http.Reque
 		app.failedValidationResponse(w, req, v.Errors)
 	}
 
-	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	movies, metadata, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
 	if err != nil {
 		app.serverErrorResponse(w, req, err)
 		return
 	}
 
-	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies, "metadata": metadata}, nil)
 	if err != nil {
 		app.serverErrorResponse(w, req, err)
 	}
